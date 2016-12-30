3334ac4b2fdd341eeb089d4aec135a02564e0262
Chelsea Skovgaard <chelseaskovgaard@gmail.com>
2016-12-30 00:11:06 -0700

    added image and class name dependencies, styled fav btn

--- webpack.config.js
+++ webpack.config.js
@@ -18,14 +18,14 @@ module.exports = {
         presets: ['es2015', 'react'],
       },
     },
-    { test: /\.(jpe?g|png|gif|svg)$/i,
-    loaders: [
-        'file?hash=sha512&digest=hex&name=[hash].[ext]',
-        'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-      ]
-    },
     { test: /\.css$/, loader: "style!css" },
-    { test: /\.scss$/, loader: "style!css!sass" }
+    { test: /\.scss$/, loader: "style!css!sass" },
+    { test: /\.(jpe?g|png|gif|svg)$/i,
+       loaders: [
+           'file?hash=sha512&digest=hex&name=[hash].[ext]',
+           'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+         ]
+       }
   ]
 },
 resolve: {
