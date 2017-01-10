d1dfcab4fbb31a7d4c35d3b3f212f8dbc9a469d0
Chelsea Skovgaard <chelseaskovgaard@gmail.com>
2017-01-10 16:59:34 -0700

    fixes routing, movie images, and styles

--- test/WatchedMovies.test.js
+++ test/WatchedMovies.test.js
@@ -2,23 +2,18 @@ import React from 'react'
 import { shallow, mount } from 'enzyme'
 import { assert, expect } from 'chai'
 import WatchedMovies from '../lib/components/WatchedMovies'
+import FavoritesMock from './helpers/FavoritesMock'
 
 const sinon = require('sinon')
 
 describe('WatchedMovies', () => {
   it('can mount with no properties', () => {
-    const wrapper = shallow(<WatchedMovies />)
+    const wrapper = shallow(<WatchedMovies favorites={FavoritesMock}/>)
   })
 
-  it('sets initial state of current movies to an empty array', () => {
-   const wrapper = shallow(<WatchedMovies />)
-   expect(wrapper.state('moviesWatched')).deep.equal([]);
-  })
-
-  it('can call componentDidMount', () => {
-    sinon.spy(WatchedMovies.prototype, 'componentDidMount')
-    const wrapper = mount(<WatchedMovies />)
-    assert.equal(WatchedMovies.prototype.componentDidMount.calledOnce, true)
-  })
+  it('renders the FavoriteMovieSimple component', () => {
+   const wrapper = mount(<WatchedMovies favorites={FavoritesMock}/>)
+   assert.equal(wrapper.find('.favorite-movie-card-container').length,1)
+ })
 
 })
